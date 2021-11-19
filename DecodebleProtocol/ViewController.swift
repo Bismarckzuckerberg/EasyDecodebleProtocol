7fbd95485ce557b7b54e7ba3c51e9a04981e8663
hasgerian <hasgerian@gmail.com>
2021-11-20 02:58:46 +0300

    Easy decode for tutorial

--- DecodebleProtocol/ViewController.swift
+++ DecodebleProtocol/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let gitUrl = URL(string: "https://api.themoviedb.org/3/movie/550?api_key=80b43b82815576429ca60bf79c604496") else { return }
+           URLSession.shared.dataTask(with: gitUrl) { (data, response
+                    , error) in
+                    guard let data = data else { return }
+                    
+                      do {
+                        let decoder = JSONDecoder()
+                        let gitData = try decoder.decode(Movies.self, from: data)
+                        print(gitData.title )
+                        
+                    } catch let err {
+                        print("Err", err)
+                 }
+           }.resume()
+    
+    
+        
+        
     }
 
+    
 
 }
+
 
---
+++ DecodebleProtocol/jsonweb.swift
@@ -0,0 +1,14 @@
+//
+//  jsonweb.swift
+//  DecodebleProtocol
+//
+//  Created by Berke T. on 20.11.2021.
+//
+
+import Foundation
+
+
+struct Movies : Decodable{
+    var title : String
+    var release_date : String
+}
